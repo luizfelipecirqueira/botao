e1a02c1feab10cc8fd06dccd88f8eba7cf94c4b9
luizfelipecirqueira <lfcs18ts@gmail.com>
2022-06-03 21:57:45 -0300

    Commit projeto de componente botão para trabalhar manipulaçao de componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Botao } from './components/Botao';
 
-function App() {
+const App = () => {
+  let textButton = "Clique no botão";
+
+  const botaoEventAction = (txt: string) => {
+    alert("Frase do app: " + txt);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Botao text={textButton} clickFn={botaoEventAction} />
     </div>
   );
 }
---
+++ src/components/Botao.tsx
@@ -0,0 +1,14 @@
+type Props = {
+    text: string;
+    clickFn: (txt: string) => void;
+}
+
+export const Botao = ({ text, clickFn }: Props) => {
+
+    const handleClick = () => {
+        clickFn("FRASE!");
+    }
+    return (
+        <button onClick={handleClick}>{text}</button>
+    );
+}
